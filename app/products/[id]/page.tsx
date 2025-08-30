082c9d9dae25bc6b677eadcd3a95a355d54bb60e
karamo10 <pabimamie@gmail.com>
2025-08-30 00:13:21 +0000

    fixed next.js type error

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,22 +1,23 @@
-import data from '@/data/products.json';
 import { Product } from '@/data/definition';
 import AddToCartButton from '@/components/button/add-to-cart-';
 
-interface ProductPageProps {
-  params: Promise<{ id: string }>; // 👈 make params a Promise
+async function getProduct(id: string): Promise<Product> {
+  const res = await fetch(`http://localhost:5000/products/${id}`);
+  if (!res.ok) throw new Error('Failed to fetch product');
+  return res.json();
 }
 
-export default async function ProductPage({ params }: ProductPageProps) {
-  const { id } = await params; // 👈 await here
-
-  const res = await fetch(`http://localhost:5000/products/${id}`);
-  if (!res.ok) throw new Error("Failed to fetch product");
-
-  const product: Product = await res.json();
- 
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const product = await getProduct(params.id);
   return (
     <div className="min-h-screen flex justify-center py-4 relative">
-      <div className="flex flex-col gap-2 items-center md:flex-row h-[400px] bg-red-800">
+      {' '}
+      <div className="flex flex-col gap-2 items-center md:flex-row h-[400px] bg-red800">
+        {' '}
         <img
           src={product.image_url}
           alt={product.name}
@@ -24,18 +25,19 @@ export default async function ProductPage({ params }: ProductPageProps) {
         />
         <div>
           <h2 className="md:text-lg font-medium mb-4">{product.name}</h2>
-
           <div className="flex items-center gap-4 mb-4">
             <p className="text-sm font-medium text-gray-800/85 md:text-sm md:font-semibold">
-              GMD {product.price}
+              GMD{product.price}
             </p>
             <AddToCartButton product={product} />
           </div>
-
-          {!product.instock && (
-            <p className="text-xs md:text-sm text-red-500 mb-2">*Out of Stock</p>
-          )}
-
+          <div>
+            {!product.instock && (
+              <p className="text-xs md:text-sm text-red-500 mb-2">
+                *Out of Stock
+              </p>
+            )}
+          </div>
           <div className="bg-green">
             <p className="text-sm font-medium mb-1">Product description</p>
             <hr className="mb-1" />
